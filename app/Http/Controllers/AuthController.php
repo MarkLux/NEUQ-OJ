5e02f7b7526ed32d9eaf9a0e806281572c9cc2d5
Hotown <hotown0515@163.com>
2016-10-25 22:11:50 +0800

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,9 +5,7 @@
  * Date: 16-10-20
  * Time: 下午10:36
  */
-
 namespace NEUQOJ\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use NEUQOJ\Common\Utils;
@@ -21,24 +19,10 @@ use NEUQOJ\Repository\Eloquent\UserRepository;
 use NEUQOJ\Services\TokenService;
 use NEUQOJ\Services\UserService;
 use Validator;
-
 class AuthController extends Controller
 {
-
-    public function register(Request $request,UserRepository $userRepository)
+    public function register(Request $request,UserService $userService)
     {
-
-        //手机和邮箱都应该检查
-        $user = $userRepository->getBy('email',$request->email)->first();
-
-        if($user!=null)
-            throw new UserExistedException();
-
-        $user = $userRepository->getBy('mobile',$request->mobile)->first();
-
-        if($user!=null)
-            throw new UserExistedException();
-
         //可以考虑修改错误信息为自定义中文
         $validator = Validator::make($request->all(),[
             'name' => 'required|max:100',
@@ -46,101 +30,63 @@ class AuthController extends Controller
             'mobile' => 'required|max:45',
             'password' => 'required|confirmed|min:6|max:20'
         ]);
-
         if($validator->fails())
         {
             $data = $validator->getMessageBag()->all();
-
             throw new FormValidatorException($data);
         }
-
+        //正则检查手机号
+        if(!Utils::IsMobile($request->mobile))
+        {
+            throw new FormValidatorException(["Valid Moblie Number"]);
+        }
+        //手机和邮箱都应该检查
+        if($userService->isUserExist('mobile',$request->mobile)||$userService->isUserExist('email',$request->email))
+            throw new UserExistedException();
         $user= [
             'name' => $request->name,
             'email' => $request->email,
             'mobile' => $request->mobile,
             'password' => bcrypt($request->password),
-            'school' => $request->school
+            'school' => $request->school?$request->school:"Unknown"
         ];
-
         /*
-         *邮件和短信验证逻辑....
+         *缺少邮箱和手机验证检查
           */
-
-        $userRepository->insert($user);
-
+        $userService->createUser($user);
         return response()->json([
             'code' => '0'
         ]);
     }
-
-    public function login(Request $request,UserRepository $userRepository,TokenRepository $tokenRepository)
+    public function login(Request $request,TokenService $tokenService,UserService $userService,UserRepository $userRepository)
     {
         $validator = Validator::make($request->all(),[
             'name' => 'required|max:100',
             'identifier' => 'required|max:100',
             'password' => 'required|min:6'
         ]);
-
         if($validator->fails())
         {
             $data = $validator->getMessageBag()->all();
-
             throw new FormValidatorException($data);
         }
-
         //正则验证登陆方式
-
-        $patternEmail = '/\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/';
-        $patternMobile ='/(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7}/';
-        if(preg_match($patternEmail,$request->identifier))
+        if(Utils::IsEmail($request->identifier))
         {
-            $user = $userRepository->getBy('email',$request->identifier)->first();
+            $user = $userService->getUser($request->identifier,'email');
         }
-        elseif (preg_match($patternMobile,$request->identifier))
+        elseif (Utils::IsMobile($request->identifier))
         {
-            $user = $userRepository->getBy('mobile',$request->identifier)->first();
+            $user = $userService->getUser($request->identifier,'mobile');
         }
         else
-            throw new FormValidatorException(['Wrong Param']);
-
+            throw new FormValidatorException(['Invalid Indentifier Format']);
         if($user == null)
             throw new UserNotExistException();
-
-//        dd($user->password);
-
         if(!Hash::check($request->password,$user->password))
             throw new PasswordErrorException();
-
         //为该登陆用户生成token
-
-        $token = $tokenRepository->getBy('user_id',$user->id)->first();
-
-        if($token == null)
-        {
-            $tokenStr = md5(uniqid());
-            $time = time();
-            $data = [
-                'user_id' => $user->id,
-                'token' => $tokenStr,
-                'created_at' => $time,
-                'updated_at' => $time,
-                'expires_at' => $time + 1728000000,
-                'ip' => $request->ip()
-            ];
-            $tokenRepository->insert($data);
-        }
-        else
-        {
-            $time = time();
-            $tokenStr = md5(uniqid());
-            $data = [
-                'token' => $tokenStr,
-                'updated_at' => $time,
-                'expires_at' => $time+1728000000
-            ];
-            $tokenRepository->update($data,$token->id);
-        }
-
+        $tokenStr = $tokenService->makeToken($user->id,$request->ip());
         return response()->json([
             'code' => '0',
             'data' => [
@@ -148,7 +94,5 @@ class AuthController extends Controller
                 'token' => $tokenStr
             ]
         ]);
-
     }
-
 }
