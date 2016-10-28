7ea27a4e6992e9e370dbc1d0eddd8a347eec0251
Hotown <hotown0515@163.com>
2016-10-28 21:37:23 +0800

    daily push

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,14 @@
 
 namespace NEUQOJ\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use NEUQOJ\Common\Utils;
+use NEUQOJ\Exceptions\PasswordErrorException;
 use NEUQOJ\Exceptions\UserExistedException;
 use NEUQOJ\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use NEUQOJ\Repository\Eloquent\UserRepository;
+use NEUQOJ\Repository\Models\User;
 use NEUQOJ\Services\UserService;
 
 
@@ -39,12 +42,13 @@ class UserController extends Controller
     public function updateUserInfo(Request $request,UserService $userService)
     {
         $user = $request->user;
+        //dd($user);
         $data = [
-            'name'=> $user->name,
-            'email' => $user->name,
-            'mobile' => $user->name,
-            'school' => $user->name,
-            'signature' => $user->name
+            'name'=> $request->name,
+            'email' => $request->email,
+            'mobile' => $request->mobile,
+            'school' => $request->school,
+            'signature' => $request->signature,
         ];
 
         $userService->updateUser($data,$user->id);
@@ -60,9 +64,41 @@ class UserController extends Controller
      */
     public function banUser(Request $request,UserService $userService)
     {
-        $id = $request->id;
-        $user = $userService->getUser($id);
-        $user->status = -1;
+        $name = $request->name;
+        $user = $userService->getUser($name,"name");
+        $data = [
+            "status" => -1,
+        ];
+        $userService->updateUser($data,$user->id);
+
+        return response()->json([
+            "code" => 0
+        ]);
+    }
+
+
+    /**
+     * 修改密码
+     *
+     * @param Request $request
+     * @param UserService $userService
+     * @return \Illuminate\Http\JsonResponse
+     * @throws PasswordErrorException
+     */
+    public function updatePassword(Request $request,UserService $userService)
+    {
+        $user = $request->user;
+        if(!Hash::check($request->oldpassword,$user->password)) {
+            throw new PasswordErrorException();
+        }
+
+        $data = [
+            "password" => bcrypt($request->newpassword)
+        ];
+        $userService->updateUser($data,$user->id);
+        return response()->json([
+            'code' => 0,
+        ]);
     }
 
 
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,11 +23,18 @@ Route::group(['middleware' => 'token'],function (){
     Route::get('/profile',function (\Illuminate\Http\Request $request){
         dd($request->user);
     });
-    Route::get('/getinfo','UserController@getUserInfo');
+
     Route::get('/test',['middleware' => 'privilege:check,create-post',function (){
         echo "You Have The Right!";
     }]);
+
+    Route::get('/getinfo','UserController@getUserInfo');
+
+    Route::post('/updateinfo','UserController@updateUserInfo');
+
+    Route::post('/ban','UserController@banUser');
+
+    Route::post('/updatepwd','UserController@updatePassword');
 });
 
 
-Route::post('/updateinfo','UserController@updateUserInfo');
